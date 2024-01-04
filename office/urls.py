7a06b514f17177f5efd32d5230ee74f95534fd78
Anish Kumar Yadav <ppf5361@gmail.com>
2024-01-04 22:51:09 +0545

    feat:clickable project details

--- office/urls.py
+++ office/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views 
+from .views import completed_projects,project_details,projects
 
 urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('projects', views.projects, name='projects'),
-    path('completed_projects', views.completed_projects, name='completed_projects'),
-    path('project_details', views.project_details, name='project_details'),
     path('about', views.about, name='about'),
+    path('completed_projects/', completed_projects, name='completed_projects'),
+    path('project/<int:project_id>/', project_details, name='project_details'),
+
 ]
--- office/views.py
+++ office/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Projects
-
 
 def contact(request):
     return render(request, 'office/contact.html')
 
-
 def projects(request):
-    return render(request, 'office/projects.html')
-
-def completed_projects(request):
-    return render(request, 'office/completed_projects.html')
-
-
-def project_details(request):
-    return render(request, 'office/project_details.html')
+    ongoing_projects = Projects.objects.filter(status='ongoing')
+    return render(request, 'office/projects.html', {'projects': ongoing_projects})
 
 def about(request):
     return render(request, 'office/about.html')
+
+def completed_projects(request):
+    completed_projects = Projects.objects.filter(status='completed')
+    return render(request, 'office/completed_projects.html', {'projects': completed_projects})
+
+def project_details(request, project_id):
+    project = get_object_or_404(Projects, id=project_id)
+    return render(request, 'office/project_details.html', {'project': project})
